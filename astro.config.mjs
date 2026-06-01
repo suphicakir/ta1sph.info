6b4336f2b0a0c7d9fe57a3414d2fea365f1cd027
suphicakir <suphicakir@gmail.com>
2026-06-01 14:22:36 +0300

    ta1sph.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://senin-site-adin.netlify.app',
+  site: 'https://ta1sph.netlify.app',
   integrations: [mdx()],
   adapter: netlify(),
   output: 'static',
